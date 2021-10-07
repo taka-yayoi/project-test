4af7a822f174fb8172ad5143d47cdc2950ba3ac0
takaaki.yayoi@databricks.com <takaaki.yayoi@databricks.com>
2021-10-07 04:06:11 +0000

    test

--- magic_command.py
+++ magic_command.py
@@ -1,2 +1,14 @@
 # Databricks notebook source
 display()
+
+# COMMAND ----------
+
+import os
+
+if os.getenv("LOCAL"):
+    # NOTE(hori-ryota): to ignore type check
+    dbutils: Any = {}
+
+# COMMAND ----------
+
+
