14ad23449e9be423837810f95c94c20848ad679d
takaaki.yayoi@databricks.com <takaaki.yayoi@databricks.com>
2021-10-07 04:38:35 +0000

    test

--- magic_command.py
+++ magic_command.py
@@ -1,16 +1,7 @@
 # Databricks notebook source
 # MAGIC %python
-# MAGIC display()
+# MAGIC dbutils.notebook.run("./test", 10)
 
 # COMMAND ----------
 
-import os
 
-if os.getenv("LOCAL"):
-    # NOTE(hori-ryota): to ignore type check
-    dbutils: Any = {}
-
-# COMMAND ----------
-
-# MAGIC %python
-# MAGIC dbutils.run("test", 10)
